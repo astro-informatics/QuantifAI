f6baacaa1882801be1bfdab9487486f1ab2cd253
Tobias Liaudat <tobiasliaudat@gmail.com>
2023-03-17 16:33:06 +0000

    add empty and identity classes

---
+++ large_scale_UQ/empty.py
@@ -0,0 +1,57 @@
+
+class Identity:
+    """Identity operator
+
+    Notes:
+        Implemented originally in optimus-primal.
+    """
+    def __init__(self):
+        """Initialises an identity operator class"""
+
+    def dir_op(self, x):
+        """Computes the forward operator of the identity class.
+
+        Args:
+            x (np.ndarray): Vector to apply identity to.
+
+        Returns:
+            np.ndarray: array of coefficients
+        """
+        return x
+
+    def adj_op(self, x):
+        """Computes the forward adjoint operator of the identity class.
+
+        Args:
+            x (np.ndarray): Vector to apply identity to.
+
+        Returns:
+            np.ndarray: array of coefficients
+        """
+        return x
+
+
+class EmptyProx:
+    def fun(self, x):
+        return 0
+
+    def prox(self, x, tau):
+        return x
+
+    def dir_op(self, x):
+        return x
+
+    def adj_op(self, x):
+        return x
+
+    beta = 1
+
+
+class EmptyGrad:
+    def fun(self, x):
+        return 0
+
+    def grad(self, x):
+        return 0
+
+    beta = 1
