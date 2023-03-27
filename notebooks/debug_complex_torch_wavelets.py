85b33222c59e777e9cb00e89a1298eac9bb54266
Tobias Liaudat <tobiasliaudat@gmail.com>
2023-03-27 22:15:54 +0100

    add debug helper

---
+++ notebooks/debug_complex_torch_wavelets.py
@@ -0,0 +1,137 @@
+
+import os
+import numpy as np
+
+import torch
+M1 = False
+
+if M1:
+    device = torch.device('mps' if torch.backends.mps.is_available() else 'cpu')
+else:
+    os.environ["CUDA_VISIBLE_DEVICES"]="1"
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    if torch.cuda.is_available():
+        print(torch.cuda.is_available())
+        print(torch.cuda.device_count())
+        print(torch.cuda.current_device())
+        print(torch.cuda.get_device_name(torch.cuda.current_device()))
+
+from functools import partial
+import math
+from tqdm import tqdm
+import time as time
+
+from skimage.metrics import peak_signal_noise_ratio as psnr
+from skimage.metrics import structural_similarity as ssim
+from torchmetrics.functional import structural_similarity_index_measure 
+from torchmetrics.functional import peak_signal_noise_ratio 
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+import scipy.io as sio
+from astropy.io import fits
+
+import large_scale_UQ as luq
+from large_scale_UQ.utils import to_numpy, to_tensor
+from convex_reg import utils as utils_cvx_reg
+
+
+# Optimisation options for the MAP estimation
+options = {"tol": 1e-5, "iter": 2500, "update_iter": 50, "record_iters": False}
+# Save param
+# repo_dir = '/disk/xray0/tl3/repos/large-scale-UQ'
+repo_dir = '/Users/tl/Documents/research/repos/proj-convex-UQ/large-scale-UQ'
+save_dir = repo_dir + '/debug/sampling-outputs/'
+
+
+img_name = 'M31'
+
+# Load img
+img_path = repo_dir + '/data/imgs/{:s}.fits'.format(img_name)
+img_data = fits.open(img_path, memmap=False)
+
+# Loading the image and cast it to float
+img = np.copy(img_data[0].data)[0,:,:].astype(np.float64)
+# Flipping data
+img = np.flipud(img)
+
+# Aliases
+x = img
+ground_truth = img
+
+
+# %%
+# Load op from X Cai
+mask_path = repo_dir + '/data/operators_masks/fourier_mask.mat'
+op_mask = sio.loadmat(mask_path)['Ma']
+
+# Matlab's reshape works with 'F'-like ordering
+mat_mask = np.reshape(np.sum(op_mask, axis=0), (256,256), order='F').astype(bool)
+
+# %%
+
+torch_img = torch.tensor(np.copy(img), dtype=torch.float, device=device).reshape((1,1) + img.shape)
+
+# %%
+dim = 256
+phi = luq.operators.MaskedFourier_torch(
+    dim=dim, 
+    ratio=0.5 ,
+    mask=mat_mask,
+    norm='ortho',
+    device=device
+)
+
+# Define X Cai noise level
+sigma = 0.0024
+
+y = phi.dir_op(torch_img).detach().cpu().squeeze().numpy()
+
+# Generate noise
+rng = np.random.default_rng(seed=0)
+n = rng.normal(0, sigma, y[y!=0].shape)
+# Add noise
+y[y!=0] += n
+
+# Observation
+torch_y = torch.tensor(np.copy(y), device=device).reshape((1,) + img.shape)
+x_init = torch.abs(phi.adj_op(torch_y))
+
+
+# Define the likelihood
+g = luq.operators.L2Norm_torch(
+    sigma=sigma,
+    data=torch_y,
+    Phi=phi,
+)
+g.beta = 1.0 / sigma ** 2
+
+
+# Define the wavelet dict
+wavs_list = ['db8'] # ["db1", "db4"] 
+levels = 4
+reg_param = 2.e-3
+# Define the l1 norm with dict psi
+# gamma = torch.max(torch.abs(psi.dir_op(y_torch))) * reg_param
+psi = luq.operators.DictionaryWv_torch(wavs_list, levels)
+
+h = luq.operators.L1Norm_torch(1., psi, op_to_coeffs=True)
+gamma = h._get_max_abs_coeffs(h.dir_op(torch.clone(torch_y))) * reg_param
+h.gamma = gamma
+h.beta = 1.0
+
+alpha = 1. / (1. + g.beta)
+
+x_hat, diagnostics = luq.optim.FB_torch(
+    x_init,
+    options=options,
+    g=g,
+    f=None,
+    h=h,
+    alpha=alpha,
+    tau=1.,
+    viewer=None
+)
+
+aa=1
