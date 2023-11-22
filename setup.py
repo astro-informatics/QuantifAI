7291e572cb6db143b792e78abc3f54176c5a11e1
Tobias Liaudat <tobiasliaudat@gmail.com>
2023-11-22 11:38:01 +0000

    update setup py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 from setuptools import setup
-
+from os import path
 import numpy
 
 # clean previous build
@@ -10,7 +10,6 @@ for root, dirs, files in os.walk("./large_scale_UQ/", topdown=False):
         if (name == "build"):
             shutil.rmtree(name)
 
-from os import path
 this_directory = path.abspath(path.dirname(__file__))
 
 def read_requirements(file):
@@ -21,7 +20,7 @@ def read_file(file):
    with open(file) as f:
         return f.read()
 
-# long_description = read_file(".pip_readme.rst")
+long_description = read_file(".pip_readme.rst")
 required = read_requirements("requirements/requirements-core.txt")
 
 include_dirs = [numpy.get_include(),]
@@ -29,24 +28,24 @@ include_dirs = [numpy.get_include(),]
 extra_link_args=[]
 
 setup(
-    classifiers=['Programming Language :: Python :: 3.6',
-                 'Programming Language :: Python :: 3.7',
-                 'Programming Language :: Python :: 3.8',
-                 'Programming Language :: Python :: 3.9',
-                 'Operating System :: OS Independent',
-                 'Intended Audience :: Developers',
-                 'Intended Audience :: Science/Research'
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research'
                  ],
-    name = "large_scale_UQ",
-    version = "0.0.1",
+    name="large_scale_UQ",
+    version="1.0.0",
     prefix='.',
     url='https://github.com/tobias-liaudat/large-scale-UQ',
     author='Tobias Liaudat et al.',
     author_email='tobiasliaudat@gmail.com',
-    license='GNU General Public License v3 (GPLv3)',
+    license='MIT license',
     install_requires=required,
-    description='Large scale convex uncertainty quantification',
-    # long_description_content_type = "text/x-rst",
-    # long_description=long_description,
+    description='Scalable Bayesian uncertainty quantification with data-driven priors for radio interferometric imaging',
+    long_description_content_type = "text/x-rst",
+    long_description=long_description,
     packages=['large_scale_UQ']
 )
