b04d4a18a0d1b512118a28d7e2f639b41e8a5767
Tobias Liaudat <tobiasliaudat@gmail.com>
2023-11-23 16:39:38 +0000

    apply black formatting

--- setup.py
+++ setup.py
@@ -7,45 +7,50 @@ import numpy
 # clean previous build
 for root, dirs, files in os.walk("./large_scale_UQ/", topdown=False):
     for name in dirs:
-        if (name == "build"):
+        if name == "build":
             shutil.rmtree(name)
 
 this_directory = path.abspath(path.dirname(__file__))
+
 
 def read_requirements(file):
     with open(file) as f:
         return f.read().splitlines()
 
+
 def read_file(file):
-   with open(file) as f:
+    with open(file) as f:
         return f.read()
+
 
 long_description = read_file(".pip_readme.rst")
 required = read_requirements("requirements/requirements-core.txt")
 
-include_dirs = [numpy.get_include(),]
+include_dirs = [
+    numpy.get_include(),
+]
 
-extra_link_args=[]
+extra_link_args = []
 
 setup(
     classifiers=[
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Operating System :: OS Independent',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research'
-                 ],
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+    ],
     name="large_scale_UQ",
     version="1.0.0",
-    prefix='.',
-    url='https://github.com/tobias-liaudat/large-scale-UQ',
-    author='Tobias Liaudat et al.',
-    author_email='tobiasliaudat@gmail.com',
-    license='MIT license',
+    prefix=".",
+    url="https://github.com/tobias-liaudat/large-scale-UQ",
+    author="Tobias Liaudat et al.",
+    author_email="tobiasliaudat@gmail.com",
+    license="MIT license",
     install_requires=required,
-    description='Scalable Bayesian uncertainty quantification with data-driven priors for radio interferometric imaging',
-    long_description_content_type = "text/x-rst",
+    description="Scalable Bayesian uncertainty quantification with data-driven priors for radio interferometric imaging",
+    long_description_content_type="text/x-rst",
     long_description=long_description,
-    packages=['large_scale_UQ']
+    packages=["large_scale_UQ"],
 )
