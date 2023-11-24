007e7ab6a1918bd3483c547bdc96530f40ceff20
Tobias Liaudat <tobiasliaudat@gmail.com>
2023-11-24 14:55:52 +0000

    add fastpixelUQ class

--- quantifai/map_uncertainty.py
+++ quantifai/map_uncertainty.py
@@ -1,6 +1,9 @@
 import numpy as np
-import logging
 import skimage as ski
+import torch
+from functools import partial
+from quantifai.utils import to_numpy, to_tensor, eval_snr
+from quantifai.operators import DictionaryWv_torch, Operation2WaveletCoeffs_torch
 
 
 def compute_UQ(MC_X_array, superpix_sizes=[32, 16, 8, 4, 1], alpha=0.05):
@@ -60,6 +63,234 @@ def compute_UQ(MC_X_array, superpix_sizes=[32, 16, 8, 4, 1], alpha=0.05):
         means_list.append(meanSample_down)
 
     return quantiles, st_dev_down, means_list
+
+
+class FastPixelUQ(object):
+    """Class to compute the fast pixel UQ
+
+    Args:
+        x_map (torch.tensor): MAP estimation with shape (1, 1, xdim, ydim)
+        myType (torch.dtype): torch type used by the torch-based potential function handle
+        potential_handle (Callable): Potential function handle. The potential should be 
+            the sum of the likelihood and prior terms. It can be set later.
+        UQ_options (dict): Parameter dictionary. If it is None, the default parameters will be used.
+    
+    """
+    def __init__(
+        self,
+        x_map,
+        myType=torch.float32,
+        potential_handle=None,
+        UQ_options=None,
+    ):
+        self.x_map = x_map
+        self.potential_handle = potential_handle
+        self.myType = myType
+        # HPD region parameters
+        self.N = None
+        self.gamma_alpha = None
+        self.tau_alpha = None
+        # Thresholded results
+        self.selected_thresh = None
+        self.thresh_img = None
+
+        if UQ_options is None:
+            print('Using default options for fast pixel UQ')
+            self.UQ_options ={
+                'alpha_prob': 0.01,         # Alpha for the HPD region
+                'wavs_list': ['db8'],       # Wavelet type for UQ maps
+                'levels': 4,                # Wavelet levels for UQ maps
+                'start_interval': [0, 10],  # Parameter for UQ map bisection
+                'iters': 5e2,               # Parameter for UQ map bisection
+                'tol': 1e-2,                # Parameter for UQ map bisection
+                'thresh_type': 'hard',      # Threshold type for fast UQ, options are 'hard' or 'soft'
+            }
+        else:
+            self.UQ_options = UQ_options
+
+        if potential_handle is not None:
+            self.potential_handle_np = lambda _x : self.potential_handle(
+                to_tensor(_x, dtype=myType)
+            ).item()
+
+        # Initialise the wavelet dictionaries
+        # Define the l1 norm with dict psi
+        self.Psi = DictionaryWv_torch(
+            self.UQ_options['wavs_list'], self.UQ_options['levels']
+        )
+        self.oper2wavelet = Operation2WaveletCoeffs_torch(Psi=self.Psi)
+
+
+    def set_quantifai_potential_fun_handle(self, lmbd, mu, CRR_model, likelihood):
+        """Set the QuantifAI potential handle.
+
+        Args:
+            lmbd (float): QuantifAI's CRR-NN lambda parameter (regularisation strength).
+            mu (float): QuantifAI's CRR-NN mu parameter.
+            CRR_model (ConvexRidgeRegularizer torch.nn.Module): CRR-NN model
+            likelihood (Grad Class): Unconstrained data-fidelity class
+
+        """
+        # Function handle for the potential
+        def _potential_handle(_x, CRR_model, mu, lmbd):
+            return (lmbd / mu) * CRR_model.cost(mu * _x) + likelihood.fun(_x)
+
+        # Evaluation of the potential
+        self.potential_handle = partial(
+            _potential_handle, CRR_model=CRR_model, mu=mu, lmbd=lmbd
+        )
+        # Evaluation of the potential in numpy
+        self.potential_handle_np = lambda _x : self.potential_handle(
+            to_tensor(_x, dtype=self.myType)
+        ).item()
+
+    def compute_HPD_region(self):
+        """Compute the high posterior density (HPD) region threshold approximation
+        """
+        if self.potential_handle is not None:
+            self.N = to_numpy(self.x_map).size
+            self.tau_alpha = np.sqrt(16*np.log(3/self.UQ_options['alpha_prob']))
+            self.gamma_alpha = self.potential_handle(self.x_map).item() + self.tau_alpha*np.sqrt(self.N) + self.N
+        else:
+            raise ValueError('The potential_handle has not been defined. Set it manually in the initialisation or call self.set_quantifai_potential_fun_handle(...).')
+
+    def compute_HPD_region_threshold(self):
+        """ Compute wavelet threshold saturating the HPD region
+        """
+        # Clone MAP estimation and cast type for wavelet operations
+        torch_map = torch.clone(self.x_map).to(torch.float64)
+
+        def _potential_to_bisect(thresh, fun_np, oper2wavelet, torch_map):
+            thresh_img = oper2wavelet.full_op_threshold_img(
+                torch_map, thresh, thresh_type=self.UQ_options['thresh_type']
+            )
+            return self.gamma_alpha - fun_np(thresh_img)
+
+        # Evaluation of the potential
+        potential_to_bisect = partial(
+            _potential_to_bisect,
+            fun_np=self.potential_handle_np,
+            oper2wavelet=self.oper2wavelet,
+            torch_map=torch_map
+        )
+
+        selected_thresh, bisec_iters = bisection_method(
+            potential_to_bisect,
+            self.UQ_options['start_interval'],
+            self.UQ_options['iters'],
+            self.UQ_options['tol'],
+            return_iters=True
+        )
+        thresh_img = self.oper2wavelet.full_op_threshold_img(
+            torch_map, selected_thresh
+        )
+
+        self.selected_thresh = selected_thresh
+        self.thresh_img = thresh_img
+
+        return thresh_img, selected_thresh
+
+
+    def compute_thresh_imgs_per_scale(self, x_gt=None, verbose=False):
+        """Compute thresholded images as a function of scale.
+
+        If a ground truth image is passed 
+
+        Args:
+            x_gt (np.ndarray): Ground truth image.
+            verbose (bool): Verbosity.
+
+        Returns:
+            modif_img_list (list of np.ndarray): Thresholded MAP image corresponding to
+                the computed HDP region as a function of scale. 
+            GT_modif_img_list (list of np.ndarray): Ground truth image where the wavelet 
+                coefficients of a single scale have been replaced by the MAP wavelet coefficients.
+                Only if a ground truth image is used.
+            SNR_at_lvl_list (list of float): Signal-to-noise-level of the thresholded image vs the 
+                MAP reconstruction as a function of scale. Only if a ground truth image is used.
+            SNR_at_lvl_map_vs_GT_list (list of float): Signal-to-noise-level of the MAP reconstruction 
+                vs the ground truth as a function of scale. Only if a ground truth image is used.
+
+        """
+        # Clone MAP estimation and cast type for wavelet operations
+        torch_map = torch.clone(self.x_map).to(torch.float64)
+        if x_gt is not None:
+            torch_x = to_tensor(x_gt).to(torch.float64)
+
+        modif_img_list = []
+        GT_modif_img_list = []
+        SNR_at_lvl_list = []
+        SNR_at_lvl_map_vs_GT_list = []
+
+        for modif_level in range(self.UQ_options['levels']+1):
+
+            op = lambda x1, x2: x2
+
+            modif_img = self.oper2wavelet.full_op_two_img(
+                torch.clone(torch_map),
+                torch.clone(self.thresh_img),
+                op,
+                level=modif_level
+            )
+            modif_img_list.append(to_numpy(modif_img))
+
+            if x_gt is not None:
+                GT_modif_img = self.oper2wavelet.full_op_two_img(
+                    torch.clone(torch_x),
+                    torch.clone(torch_map),
+                    op,
+                    level=modif_level
+                )
+                if verbose:
+                    print('SNR (thresh vs MAP) at lvl {:d}: {:f}'.format(
+                        modif_level, eval_snr(to_numpy(torch_map), to_numpy(modif_img)))
+                    )
+                    print('SNR (MAP vs GT) at lvl {:d}: {:f}'.format(
+                        modif_level, eval_snr(x_gt, to_numpy(GT_modif_img)))
+                    )
+                
+                GT_modif_img_list.append(to_numpy(GT_modif_img))
+                SNR_at_lvl_list.append(
+                    eval_snr(x_gt, to_numpy(modif_img))
+                )
+                SNR_at_lvl_map_vs_GT_list.append(
+                    eval_snr(x_gt, to_numpy(GT_modif_img))
+                )
+        
+        if x_gt is not None:
+            return modif_img_list, GT_modif_img_list, SNR_at_lvl_list, SNR_at_lvl_map_vs_GT_list
+        else:
+            return modif_img_list
+
+
+    def run(self, x_gt, verbose=False):
+        """Run the fast pixel UQ
+        
+        1st Compute HPD region.
+        2nd Compute the threshold saturating the HPD region.
+        3rd Compute the thresholded images per scale.
+        
+        
+        Args:
+            x_gt (np.ndarray): Ground truth image.
+            verbose (bool): Verbosity.
+
+        Returns:
+            modif_img_list (list of np.ndarray): Thresholded MAP image corresponding to
+                the computed HDP region as a function of scale. 
+            GT_modif_img_list (list of np.ndarray): Ground truth image where the wavelet 
+                coefficients of a single scale have been replaced by the MAP wavelet coefficients.
+                Only if a ground truth image is used.
+            SNR_at_lvl_list (list of float): Signal-to-noise-level of the thresholded image vs the 
+                MAP reconstruction as a function of scale. Only if a ground truth image is used.
+            SNR_at_lvl_map_vs_GT_list (list of float): Signal-to-noise-level of the MAP reconstruction 
+                vs the ground truth as a function of scale. Only if a ground truth image is used.
+
+        """
+        self.compute_HPD_region()
+        _, _ = self.compute_HPD_region_threshold()
+        return self.compute_thresh_imgs_per_scale(x_gt, verbose)
+
 
 
 def bisection_method(function, start_interval, iters, tol, return_iters=False):
