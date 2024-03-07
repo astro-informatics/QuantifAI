56d1093b2de24e73a43b8e2fb971980339cadc75
Tobias Liaudat <tobiasliaudat@gmail.com>
2024-03-07 15:55:34 +0000

    apply black formatting

--- quantifai/operators.py
+++ quantifai/operators.py
@@ -337,15 +337,16 @@ class MaskedFourier_torch(torch.nn.Module):
         return torch.fft.ifft2(x, norm=self.norm)
 
 
-class NUFFT2D_Torch():
-    """NUFFT implementation using a Kaiser-Bessel kernel for interpolation. 
-    Implemented with TF operations. Only able to do the FFT on the last 2 axes 
-    of the tensors provided. Slower than using the numpy_function on the np 
+class NUFFT2D_Torch:
+    """NUFFT implementation using a Kaiser-Bessel kernel for interpolation.
+    Implemented with TF operations. Only able to do the FFT on the last 2 axes
+    of the tensors provided. Slower than using the numpy_function on the np
     based operations.
     """
+
     def __init__(self):
         pass
-        
+
     def plan(self, uv, Nd, Kd, Jd, batch_size):
         # saving some values
         self.Nd = Nd
@@ -353,14 +354,14 @@ class NUFFT2D_Torch():
         self.Jd = Jd
         self.batch_size = batch_size
         self.n_measurements = len(uv)
-        
+
         self.K_norm = max(Kd[0], Kd[1])
-        gridsize = 2*np.pi / self.K_norm
+        gridsize = 2 * np.pi / self.K_norm
         k = (uv + np.pi) / gridsize
-        
+
         # calculating coefficients for interpolation
         indices = []
-        values =  []
+        values = []
         for i in range(len(uv)):
             ind, vals = self.calculate_kaiser_bessel_coef(k[i], i, Jd)
             indices.append(ind)
@@ -371,42 +372,68 @@ class NUFFT2D_Torch():
 
         self.indices = indices
         # check if indices are within bounds, otherwise suppress them and raise warning
-        if np.any(indices[:,2:] < 0) or np.any(indices[:,2] >= Kd[0]) or np.any(indices[:,3] >= Kd[1]):
-            sel_out_bounds = np.any([np.any(indices[:,2:] < 0, axis=1), indices[:,2] >= Kd[0], indices[:,3] >= Kd[1]], axis=0)
-            print(f"some values lie out of the interpolation array, these are not used, check baselines")
+        if (
+            np.any(indices[:, 2:] < 0)
+            or np.any(indices[:, 2] >= Kd[0])
+            or np.any(indices[:, 3] >= Kd[1])
+        ):
+            sel_out_bounds = np.any(
+                [
+                    np.any(indices[:, 2:] < 0, axis=1),
+                    indices[:, 2] >= Kd[0],
+                    indices[:, 3] >= Kd[1],
+                ],
+                axis=0,
+            )
+            print(
+                f"some values lie out of the interpolation array, these are not used, check baselines"
+            )
             indices[sel_out_bounds] = 0
             values[sel_out_bounds] = 0
-        
-        # repeating the values and indices to match the batch_size 
-        batch_indices = np.tile(indices[:,-2:], [batch_size, 1])
-        batch_indicators = np.repeat(np.arange(batch_size), (len(values)))
-        batch_indices = np.hstack((batch_indicators[:,None], batch_indices))
-        
-        self.flat_batch_indices = torch.LongTensor(np.ravel_multi_index(batch_indices.T, (batch_size, Kd[0], Kd[1])))
-        
-        self.batch_indices = list(torch.LongTensor(batch_indices).T)
-        self.batch_values = np.tile(values, [batch_size,1]).astype(np.float32).reshape(self.batch_size, self.n_measurements, self.Jd[0]*self.Jd[1])
 
+        # repeating the values and indices to match the batch_size
+        batch_indices = np.tile(indices[:, -2:], [batch_size, 1])
+        batch_indicators = np.repeat(np.arange(batch_size), (len(values)))
+        batch_indices = np.hstack((batch_indicators[:, None], batch_indices))
+
+        self.flat_batch_indices = torch.LongTensor(
+            np.ravel_multi_index(batch_indices.T, (batch_size, Kd[0], Kd[1]))
+        )
+
+        self.batch_indices = list(torch.LongTensor(batch_indices).T)
+        self.batch_values = (
+            np.tile(values, [batch_size, 1])
+            .astype(np.float32)
+            .reshape(self.batch_size, self.n_measurements, self.Jd[0] * self.Jd[1])
+        )
 
         # determine scaling based on iFT of the KB kernel
-        J = Jd[0] 
-        beta = 2.34*J
-        s_kb = lambda x: np.sinc(np.sqrt((np.pi *x *J)**2 - (2.34*J)**2 +0j)/np.pi)
+        J = Jd[0]
+        beta = 2.34 * J
+        s_kb = lambda x: np.sinc(
+            np.sqrt((np.pi * x * J) ** 2 - (2.34 * J) ** 2 + 0j) / np.pi
+        )
 
-        xx_1 = (np.arange(Kd[0])/Kd[0] -.5)[(Kd[0]-Nd[0])//2:(Kd[0]-Nd[0])//2 + Nd[0]]
-        xx_2 = (np.arange(Kd[1])/Kd[1] -.5)[(Kd[1]-Nd[1])//2:(Kd[1]-Nd[1])//2 + Nd[1]]
-        
+        xx_1 = (np.arange(Kd[0]) / Kd[0] - 0.5)[
+            (Kd[0] - Nd[0]) // 2 : (Kd[0] - Nd[0]) // 2 + Nd[0]
+        ]
+        xx_2 = (np.arange(Kd[1]) / Kd[1] - 0.5)[
+            (Kd[1] - Nd[1]) // 2 : (Kd[1] - Nd[1]) // 2 + Nd[1]
+        ]
+
         sa_1 = s_kb(xx_1).real
         sa_2 = s_kb(xx_2).real
-        
-        self.scaling = (sa_1.reshape(-1,1) * sa_2.reshape(1,-1)).reshape(1, Nd[0], Nd[1])
+
+        self.scaling = (sa_1.reshape(-1, 1) * sa_2.reshape(1, -1)).reshape(
+            1, Nd[0], Nd[1]
+        )
         self.scaling = torch.tensor(self.scaling, dtype=torch.complex64)
         self.forward = self.dir_op
         self.adjoint = self.adj_op
-    
+
     @staticmethod
-    def calculate_kaiser_bessel_coef(k, i, Jd=(6,6)):
-        """Calculate the Kaiser-Bessel kernel coefficients for a 2d grid for the neighbouring pixels. 
+    def calculate_kaiser_bessel_coef(k, i, Jd=(6, 6)):
+        """Calculate the Kaiser-Bessel kernel coefficients for a 2d grid for the neighbouring pixels.
 
         Args:
             k (float,float): location of the point to be interpolated
@@ -418,39 +445,41 @@ class NUFFT2D_Torch():
             values (list): list of the calculated coefficients
         """
 
-        k = k.reshape(-1,1)
-        J = Jd[0]//2
+        k = k.reshape(-1, 1)
+        J = Jd[0] // 2
         a = np.array(np.meshgrid(range(-J, J), range(-J, J))).reshape(2, -1)
-        a += (k % 1 >0.5) # corrects to the closest 6 pixels
-        indices = (k.astype(int) + a)
+        a += k % 1 > 0.5  # corrects to the closest 6 pixels
+        indices = k.astype(int) + a
 
         J = Jd[0]
 
-        beta = 2.34*J
-        norm = J 
+        beta = 2.34 * J
+        norm = J
 
         # for 2d do the interpolation 2 times, once in each direction
-        u =  k.reshape(2,1) - indices
-        values1 = iv(0, beta*np.sqrt(1 +0j - (2*u[0]/Jd[0])**2)).real / J 
-        values2 = iv(0, beta*np.sqrt(1 +0j - (2*u[1]/Jd[0])**2)).real / J 
+        u = k.reshape(2, 1) - indices
+        values1 = iv(0, beta * np.sqrt(1 + 0j - (2 * u[0] / Jd[0]) ** 2)).real / J
+        values2 = iv(0, beta * np.sqrt(1 + 0j - (2 * u[1] / Jd[0]) ** 2)).real / J
         values = values1 * values2
-        
-        indices = np.vstack((
-                np.zeros(indices.shape[1]), 
-                np.repeat(i, indices.shape[1]), indices[0], indices[1])
-                ).astype(int)
+
+        indices = np.vstack(
+            (
+                np.zeros(indices.shape[1]),
+                np.repeat(i, indices.shape[1]),
+                indices[0],
+                indices[1],
+            )
+        ).astype(int)
 
         return indices.T, values
 
-
     def dir_op(self, xx):
         xx = torch.tensor(xx, dtype=torch.complex64)
-        xx = xx/self.scaling
+        xx = xx / self.scaling
         xx = self._pad(xx)
         kk = self._xx2kk(xx) / self.K_norm
         k = self._kk2k(kk)
         return k
-
 
     def adj_op(self, k):
         # split real and imaginary parts because complex operations not defined for sparseTensors
@@ -461,50 +490,69 @@ class NUFFT2D_Torch():
         xx = xx / self.scaling
 
         return xx
-    
+
     def _kk2k(self, kk):
         """interpolates of the grid to non uniform measurements"""
-        
+
         print(kk.shape)
-        
-        return (kk[self.batch_indices].reshape(self.batch_size, self.n_measurements, self.Jd[0]*self.Jd[1]) * self.batch_values).sum(axis=-1)
-    
+
+        return (
+            kk[self.batch_indices].reshape(
+                self.batch_size, self.n_measurements, self.Jd[0] * self.Jd[1]
+            )
+            * self.batch_values
+        ).sum(axis=-1)
+
     def _k2kk(self, k):
         """convolutes measurements to oversampled fft grid"""
-        interp = (k.reshape(self.batch_size, self.n_measurements, 1) * self.batch_values).reshape(-1)
-        
-        kk_flat = torch.zeros(self.batch_size * self.Kd[0] * self.Kd[1], dtype=torch.complex64)
-        kk_flat.scatter_add_(0, self.flat_batch_indices, interp )
+        interp = (
+            k.reshape(self.batch_size, self.n_measurements, 1) * self.batch_values
+        ).reshape(-1)
+
+        kk_flat = torch.zeros(
+            self.batch_size * self.Kd[0] * self.Kd[1], dtype=torch.complex64
+        )
+        kk_flat.scatter_add_(0, self.flat_batch_indices, interp)
 
         return kk_flat.reshape(self.batch_size, self.Kd[0], self.Kd[1])
-    
+
     @staticmethod
     def _kk2xx(kk):
         """from 2d fourier space to 2d image space"""
-        return torch.fft.ifftshift(torch.fft.ifft2(torch.fft.ifftshift(kk, dim=(-2,-1))), dim=(-2,-1)) 
+        return torch.fft.ifftshift(
+            torch.fft.ifft2(torch.fft.ifftshift(kk, dim=(-2, -1))), dim=(-2, -1)
+        )
 
     @staticmethod
     def _xx2kk(xx):
         """from 2d fourier space to 2d image space"""
-        return torch.fft.fftshift(torch.fft.fft2(torch.fft.fftshift(xx, dim=(-2,-1))), dim=(-2,-1))
-    
+        return torch.fft.fftshift(
+            torch.fft.fft2(torch.fft.fftshift(xx, dim=(-2, -1))), dim=(-2, -1)
+        )
+
     def _pad(self, x):
         """pads x to go from Nd to Kd"""
-        return torch.nn.functional.pad(x, (
-            (self.Kd[1]-self.Nd[1])//2, (self.Kd[1]-self.Nd[1])//2,
-            (self.Kd[0]-self.Nd[0])//2, (self.Kd[0]-self.Nd[0])//2,
-            0, 0,
-            )
+        return torch.nn.functional.pad(
+            x,
+            (
+                (self.Kd[1] - self.Nd[1]) // 2,
+                (self.Kd[1] - self.Nd[1]) // 2,
+                (self.Kd[0] - self.Nd[0]) // 2,
+                (self.Kd[0] - self.Nd[0]) // 2,
+                0,
+                0,
+            ),
         )
-    
+
     def _unpad(self, x):
         """unpads x to go from  Kd to Nd"""
         return x[
             :,
-            (self.Kd[0]-self.Nd[0])//2: (self.Kd[0]-self.Nd[0])//2 +self.Nd[0],
-            (self.Kd[1]-self.Nd[1])//2: (self.Kd[1]-self.Nd[1])//2 +self.Nd[1]
-            ] 
-
+            (self.Kd[0] - self.Nd[0]) // 2 : (self.Kd[0] - self.Nd[0]) // 2
+            + self.Nd[0],
+            (self.Kd[1] - self.Nd[1]) // 2 : (self.Kd[1] - self.Nd[1]) // 2
+            + self.Nd[1],
+        ]
 
 
 class L2Norm_torch(torch.nn.Module):
@@ -685,7 +733,9 @@ class Wavelets_torch(torch.nn.Module):
         if wav == "self":
             self.shape = shape
             if shape is None:
-                raise ValueError("`self` wavelet type requires the shape of the images as input.")
+                raise ValueError(
+                    "`self` wavelet type requires the shape of the images as input."
+                )
 
             self.adj_op(self.dir_op(torch.ones(self.shape)))
         else:
@@ -716,7 +766,9 @@ class Wavelets_torch(torch.nn.Module):
                     x.squeeze(1), wavelet=self.wav, level=self.levels, mode=self.mode
                 )
             else:
-                return ptwt.wavedec2(x, wavelet=self.wav, level=self.levels, mode=self.mode)
+                return ptwt.wavedec2(
+                    x, wavelet=self.wav, level=self.levels, mode=self.mode
+                )
 
     def adj_op(self, coeffs):
         """Evaluates the forward adjoint abstract wavelet transform of x
@@ -734,7 +786,6 @@ class Wavelets_torch(torch.nn.Module):
             return torch.reshape(coeffs, self.shape)
         else:
             return ptwt.waverec2(coeffs, wavelet=self.wav).squeeze(1)
-
 
 
 class DictionaryWv_torch(torch.nn.Module):
@@ -799,7 +850,6 @@ class DictionaryWv_torch(torch.nn.Module):
         return out / len(self.wavelet_list)
 
 
-
 class L1Norm_torch(torch.nn.Module):
     """This class computes the proximity operator of the l2 ball.
 
@@ -847,7 +897,6 @@ class L1Norm_torch(torch.nn.Module):
         """Applies operation to all coefficients in ptwt structure."""
         # Iterate over the wavelet dictionaries
         for wav_i in range(self.num_wavs):
-
             if torch.is_tensor(coeffs[wav_i]):
                 # case of `self` wavelets
                 coeffs[wav_i] = op(coeffs[wav_i])
@@ -856,7 +905,9 @@ class L1Norm_torch(torch.nn.Module):
                 coeffs[wav_i][0] = op(coeffs[wav_i][0])
                 # Iterate over the wavelet decomp and apply op
                 for it1 in range(1, len(coeffs[0])):
-                    coeffs[wav_i][it1] = tuple([op(elem) for elem in coeffs[wav_i][it1]])
+                    coeffs[wav_i][it1] = tuple(
+                        [op(elem) for elem in coeffs[wav_i][it1]]
+                    )
 
         return coeffs
 
@@ -880,7 +931,6 @@ class L1Norm_torch(torch.nn.Module):
         """
         # Iterate over the wavelet dictionaries
         for wav_i in range(self.num_wavs):
-
             if torch.is_tensor(coeffs1[wav_i]) or torch.is_tensor(coeffs2[wav_i]):
                 # case of `self` wavelets
                 coeffs1[wav_i] = op(coeffs1[wav_i], coeffs2[wav_i])
@@ -914,7 +964,6 @@ class L1Norm_torch(torch.nn.Module):
         max_val = []
         # Iterate over the wavelet dictionaries
         for wav_i in range(self.num_wavs):
-
             if torch.is_tensor(coeffs[wav_i]):
                 # case of `self` wavelets
                 max_val.append(torch.max(torch.abs(coeffs[wav_i])))
@@ -962,7 +1011,6 @@ class L1Norm_torch(torch.nn.Module):
         loss = 0
         # Iterate over the wavelet dictionaries
         for wav_i in range(self.num_wavs):
-
             if torch.is_tensor(coeffs[wav_i]):
                 # case of `self` wavelets
                 loss += self._fun(coeffs[wav_i])
@@ -1143,7 +1191,6 @@ class Operation2WaveletCoeffs_torch(torch.nn.Module):
         """Applies operation to all coefficients in ptwt structure."""
         # Iterate over the wavelet dictionaries
         for wav_i in range(self.num_wavs):
-
             if torch.is_tensor(coeffs[wav_i]):
                 # case of `self` wavelets
                 coeffs[wav_i] = op(coeffs[wav_i])
@@ -1152,7 +1199,9 @@ class Operation2WaveletCoeffs_torch(torch.nn.Module):
                 coeffs[wav_i][0] = op(coeffs[wav_i][0])
                 # Iterate over the wavelet decomp and apply op
                 for it1 in range(1, len(coeffs[0])):
-                    coeffs[wav_i][it1] = tuple([op(elem) for elem in coeffs[wav_i][it1]])
+                    coeffs[wav_i][it1] = tuple(
+                        [op(elem) for elem in coeffs[wav_i][it1]]
+                    )
 
         return coeffs
 
@@ -1207,7 +1256,6 @@ class Operation2WaveletCoeffs_torch(torch.nn.Module):
         """
         # Iterate over the wavelet dictionaries
         for wav_i in range(self.num_wavs):
-
             if torch.is_tensor(coeffs1[wav_i]) or torch.is_tensor(coeffs2[wav_i]):
                 # case of `self` wavelets
                 coeffs1[wav_i] = op(coeffs1[wav_i], coeffs2[wav_i])
@@ -1251,7 +1299,6 @@ class Operation2WaveletCoeffs_torch(torch.nn.Module):
         else:
             # Iterate over the wavelet dictionaries
             for wav_i in range(self.num_wavs):
-
                 if torch.is_tensor(coeffs1[wav_i]) or torch.is_tensor(coeffs2[wav_i]):
                     # case of `self` wavelets
                     coeffs1[wav_i] = op(coeffs1[wav_i], coeffs2[wav_i])
@@ -1287,7 +1334,6 @@ class Operation2WaveletCoeffs_torch(torch.nn.Module):
         max_val = []
         # Iterate over the wavelet dictionaries
         for wav_i in range(self.num_wavs):
-
             if torch.is_tensor(coeffs[wav_i]):
                 # case of `self` wavelets
                 max_val.append(torch.max(torch.abs(coeffs[wav_i])))
@@ -1500,4 +1546,3 @@ class Operation2WaveletCoeffs_torch(torch.nn.Module):
             Forward adjoint regularisation operator applied to x
         """
         return self.Psi.adj_op(x)
-
--- quantifai/simulations.py
+++ quantifai/simulations.py
@@ -39,19 +39,18 @@ import os
 import matplotlib.pyplot as plt
 
 
-
 def generate_random_empty_ms(
-        msname='empty',
-        dtime=240,
-        synthesis_time=4,
-        direction=None,
-        f0=None,
-    ):
+    msname="empty",
+    dtime=240,
+    synthesis_time=4,
+    direction=None,
+    f0=None,
+):
     """creates empty meerkat measurement set
 
     Example of `direction`: `"13h18m54.86s,-15d36m04.25s"`
     Example of `f0`: `1400`
-    
+
     """
     import simms
     from casatasks import exportuvfits
@@ -59,19 +58,17 @@ def generate_random_empty_ms(
     from astropy.coordinates import SkyCoord
     import astropy.units as u
 
-
     if direction is None:
-        ra = np.random.rand()*360
-        dec = -30 + np.random.rand()*80 - 40
-        coord = SkyCoord(ra=ra*u.deg,dec=dec*u.deg)
-        direction = coord.to_string('hmsdms',precision=2).replace(' ',',')
+        ra = np.random.rand() * 360
+        dec = -30 + np.random.rand() * 80 - 40
+        coord = SkyCoord(ra=ra * u.deg, dec=dec * u.deg)
+        direction = coord.to_string("hmsdms", precision=2).replace(" ", ",")
     if f0 is None:
-        f0 = int(800 + 700*np.random.rand())
+        f0 = int(800 + 700 * np.random.rand())
 
     simms_call = f"""simms -dir J2000,{direction} -os -T meerkat -dt {dtime} -st {synthesis_time} -nc 10 -f0 {f0}MHz -df 10MHz -pl XX YY -n {msname}.ms"""
     print(simms_call)
     os.system(simms_call)
-
 
     # Export UV data into a fits file
     exportuvfits(f"{msname}.ms", fitsfile=f"{msname}_uv.fits")
@@ -79,8 +76,8 @@ def generate_random_empty_ms(
     # Load fits file
     uvfits = fits.open(f"{msname}_uv.fits")
 
-    uu_data = uvfits[0].data['UU']
-    vv_data = uvfits[0].data['VV']
+    uu_data = uvfits[0].data["UU"]
+    vv_data = uvfits[0].data["VV"]
 
     # Normalize UV data to [-pi, pi]
     uu_data = (uu_data - uu_data.min()) / (uu_data.max() - uu_data.min())
@@ -91,20 +88,17 @@ def generate_random_empty_ms(
 
     # Save dict
     save_dict = {
-        'uu' : uu_data,
-        'vv' : vv_data,
-        'simms_call' : simms_call,
+        "uu": uu_data,
+        "vv": vv_data,
+        "simms_call": simms_call,
     }
     np.save(f"{msname}_uv_only.npy", save_dict, allow_pickle=True)
 
-
     # Plot UV coverage
-    plt.figure(figsize=(10,8), dpi=300)
+    plt.figure(figsize=(10, 8), dpi=300)
     s = (np.arange(len(uu_data)) + 1) / (len(uu_data))
     plt.scatter(uu_data, vv_data, s=s, alpha=0.75)
     plt.xlabel(r"u", fontsize=16)
     plt.ylabel(r"v", fontsize=16)
     plt.savefig(f"{msname}_uv_coverage.jpg")
     plt.close()
-
-
